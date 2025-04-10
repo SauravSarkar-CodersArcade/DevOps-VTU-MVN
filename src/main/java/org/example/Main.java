84ca5533f3e3759109d44dd9ff78fc19a78c3e78
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2025-04-10 17:54:22 +0530

    Added all files till mvn life cycles site & deploy. All finalized

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello! This is Saurav...!!!");
+        System.out.println("Welcome To The DevOps VTU Exp 2");
+        System.out.println("Commit To Achieve");
+        System.out.println("Happy Learning");
     }
 }
